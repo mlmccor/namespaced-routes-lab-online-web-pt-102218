bf45990a7b319c08cb217af3c001f6445e28f27b
Matthew McCormack <mmccormack1182@gmail.com>
2019-04-13 19:06:42 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :songs, only: [:index, :show, :new, :create]
+  resources :artists, only: [:index, :show, :new, :create]
+
+  resources :preferences, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :preferences, only: [:index]
+  end
 end
